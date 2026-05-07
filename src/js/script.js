1d7fb63e1f21b545109fe45a1ed66da64a0829ce
Vinicius <viniciusfuentescavalcanti@gmail.com>
2026-05-07 11:43:14 -0300

    fix: Corrigindo codigo

--- src/js/script.js
+++ src/js/script.js
@@ -19,7 +19,7 @@ window.onload = function(){
 document.addEventListener('DOMContentLoaded', () =>{
     const menuIcone = document.getElementById("menu-icone");
     const navMenu = document.querySelector("nav");
-    if(menIcone && navMenu){
+    if(menuIcone && navMenu){
         menuIcone.onclick=() =>{
             navMenu.classList.toggle("active");
             menuIcone.classList.toggle("open");
