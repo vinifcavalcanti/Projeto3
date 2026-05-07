bfe08d4deb0e23b655d35ed07938a1fad2938d71
Vinicius <viniciusfuentescavalcanti@gmail.com>
2026-05-07 10:41:08 -0300

    feat: Arrumando CSS e JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,15 @@
 function trocar(cor){
     document.body.style.background=cor;
 }
+window.onload = function(){
+    const intro=document.getElementById("inicio");
+
+    setTimeout(()=>{
+        intro.classList.add("efeito-out")
+        setTimeout(()=>{
+            intro.style.display="none";
+            content.classList.remove("hidden");
+            content.classList.add("efeito-in")
+        },1000)
+    },3000)
+}
